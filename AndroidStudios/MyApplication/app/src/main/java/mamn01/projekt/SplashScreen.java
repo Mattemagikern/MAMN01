c968219329308d5565e6a63933cc798dc341c205
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2017-03-31 12:03:32 +0200

    Create splashscreen
    Splashscreen is active 3 seconds before going to mainactivity
    closes #17

---
+++ AndroidStudios/MyApplication/app/src/main/java/mamn01/projekt/SplashScreen.java
@@ -0,0 +1,34 @@
+package mamn01.projekt;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+            /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, 3000);
+
+    }
+
+}
