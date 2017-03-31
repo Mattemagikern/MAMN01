35eae9e50159dd993f3690d90b95b2d8ee38e8fc
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2017-03-31 17:07:54 +0200

    connect hugboard to backend

--- AndroidStudios/MyApplication/app/src/main/java/mamn01/projekt/RatingActivity.java
+++ AndroidStudios/MyApplication/app/src/main/java/mamn01/projekt/RatingActivity.java
@@ -9,6 +9,17 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class RatingActivity extends ListActivity {
 
     String[] values = { "1. Nille\t\t50 Hugpoint", "2. Hanna\t\t30 Hugpoint", "3. Hanna\t\t10 Hugpoint", "4. Måns\t\t-10 Hugpoint" };
@@ -21,9 +32,33 @@ public class RatingActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating);
 
-        // parameters are (Context, layout for the row, and the array of data)
-        setListAdapter(new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, values));
+
+       // Instantiate the RequestQueue.
+        String url = "http://shapeapp.se/mamn01/?action=getHugboard";
+        final ArrayList<String> hugboard = new ArrayList<String>();
+        JsonArrayRequest jsObjRequest = new JsonArrayRequest
+            (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                @Override
+                public void onResponse(JSONArray response) {
+                    try {
+                        for(int i = 0; i < response.length(); i++){
+                            JSONObject o = (JSONObject) response.get(i);
+                            hugboard.add(String.valueOf(i +1) + ". " + o.getString("name") + "\t\t" + o.getString("hugpoints") + "points");
+                        }
+                        setListAdapter(new ArrayAdapter<String>(RatingActivity.this,
+                                android.R.layout.simple_list_item_1, hugboard));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("Error: " + error.getMessage());
+            }
+        });
+        // Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsObjRequest);
     }
 
     @Override
